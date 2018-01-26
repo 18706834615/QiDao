561d676488179a843729f2c45e780cabc31eb368
18706834615 <1540532869@qq.com>
2018-01-26 17:54:40 +0800

    提交许可

--- QiDao/ViewController.swift
+++ QiDao/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("试一下")
-        print("试一下")print("试一下")
+        print("试一下")
         print("试一下")
     }
 
