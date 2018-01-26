757389a92822e90460552a7a2ce7058d0f15721f
18706834615 <1540532869@qq.com>
2018-01-26 17:55:14 +0800

    嗯嗯

--- QiDao/ViewController.swift
+++ QiDao/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("试一下")
-        print("试一下")
-        print("试一下")
+      
     }
 
     override func didReceiveMemoryWarning() {
